d1a9dbe379097ccfa92d75161dbb852002cb9778
Roshan-ac <roshanacharya3636@gmail.com>
2024-01-11 01:11:41 +0545

    fixed

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,8 @@ import { Sidebar } from "@/components/Sidebar";
 
 export default function Home() {
   return (
-    <main className="">
-      <Navbar />
       <div className="flex w-full justify-end">
-        <Sidebar />
         <main className="h-full w-[80%] bg-secondaryBackground text-primaryText"></main>
       </div>
-    </main>
   );
 }
