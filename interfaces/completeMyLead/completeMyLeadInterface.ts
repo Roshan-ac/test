6c856963c3c3551e4bbbf83bfe9665e34484d165
Roshan-ac <roshanacharya3636@gmail.com>
2024-02-28 16:59:44 +0545

    fixing completemylead api

---
+++ interfaces/completeMyLead/completeMyLeadInterface.ts
@@ -0,0 +1,16 @@
+export interface CompleteMyLeadInterface {
+    customerid: string,
+    leadid: string,
+    isCompleted: 1,
+    pickupdate: string,
+    pickuptime: string,
+    owneraddress: string,
+    pincode: string,
+    paymentmode:string,
+    upino:string,
+    bankacno: string,
+    bankifsccode: string,
+    bankbeneficiaryname: string,
+    bankname: string
+}
+  
