9cc32240301d8f6a042ce55baeb2699bde58d0f4
samrat-gh <samratghimire32@gmail.com>
2024-05-14 17:58:54 +0545

    update loader to img tag

--- app/loading.tsx
+++ app/loading.tsx
@@ -3,13 +3,7 @@ import Image from "next/image";
 export default function Loading() {
   return (
     <div className=" flex h-[100vh] w-[100vw] items-center justify-center bg-secondaryBackground">
-      <Image
-        alt=""
-        height={80}
-        width={80}
-        src={"/images/loaderBar.gif"}
-        unoptimized={true}
-      />
+      <img alt="" height={80} width={80} src="/images/loaderBar.gif" />
     </div>
   );
 }
