b3cfe248206156c89dda14a0ff5c890c86ffd9a8
Roshan-ac <roshanacharya3636@gmail.com>
2024-05-22 14:02:44 +0545

    fixed build issues

--- components/LoadingUi.tsx
+++ components/LoadingUi.tsx
@@ -1,14 +1,9 @@
-"use client";
-import { cardio } from "ldrs";
-import { useEffect } from "react";
+import Image from "next/image";
 
 export default function LoadingUI() {
-  useEffect(() => {
-    cardio.register();
-  }, []);
   return (
     <div className=" flex h-[100vh] w-full items-center justify-center">
-      <l-cardio size="50" stroke="4" speed="2" color="white"></l-cardio>
+    <Image src={'/images/loading.gif'} alt="" height={50} width={50} />
     </div>
   );
 }
